7ec20d5e5c62656f7ece9a75b5955b536b804870
Ain Ghazal <ainghazal42@gmail.com>
2022-03-03 22:15:54 +0100

    docs

--- proxy.go
+++ proxy.go
@@ -6,19 +6,22 @@ import (
 	"github.com/ainghazal/minivpn/vpn"
 )
 
+// ListenAnListenAndServeSocks configures a vpn dialer, and configures and runs a
+// socks5 server to use dialer.DialContext. The vpn dialer will initialize the tunnel
+// upon receiving the first proxied request, and will reuse the same session
+// for all further requests.
 func ListenAndServeSocks(opts *vpn.Options) {
 	dialer := vpn.NewDialerFromOptions(opts)
 
-	// Create a SOCKS5 server
 	conf := &socks5.Config{
 		Dial: dialer.DialContext,
 	}
+
 	server, err := socks5.New(conf)
 	if err != nil {
 		panic(err)
 	}
 
-	// Create SOCKS5 proxy on localhost port 8000
 	if err := server.ListenAndServe("tcp", "127.0.0.1:8000"); err != nil {
 		panic(err)
 	}
