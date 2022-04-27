63e2005536cdc7657c231a5c028e7ed1fffba9bb
Ain Ghazal <ainghazal42@gmail.com>
2022-04-27 21:57:54 +0200

    make socks5 port configurable

--- proxy.go
+++ proxy.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	socks5 "github.com/armon/go-socks5"
+	"fmt"
+	"net"
+	"os"
 
 	"github.com/ainghazal/minivpn/vpn"
+	socks5 "github.com/armon/go-socks5"
+)
+
+const (
+	socksPort = "8080"
+	socksIP   = "127.0.0.1"
 )
 
 // ListenAndServeSocks configures a vpn dialer, and configures and runs a
@@ -11,18 +19,26 @@ import (
 // upon receiving the first proxied request, and will reuse the same session
 // for all further requests.
 func ListenAndServeSocks(opts *vpn.Options) {
+	port := os.Getenv("LPORT")
+	if port == "" {
+		port = socksPort
+	}
+	ip := os.Getenv("LHOST")
+	if ip == "" {
+		ip = socksIP
+	}
 	dialer := vpn.NewDialerFromOptions(opts)
-
 	conf := &socks5.Config{
 		Dial: dialer.DialContext,
 	}
-
 	server, err := socks5.New(conf)
 	if err != nil {
 		panic(err)
 	}
 
-	if err := server.ListenAndServe("tcp", "127.0.0.1:8000"); err != nil {
+	addr := net.JoinHostPort(ip, port)
+	fmt.Printf("[+] Started socks5 proxy at %s\n", addr)
+	if err := server.ListenAndServe("tcp", addr); err != nil {
 		panic(err)
 	}
 }
