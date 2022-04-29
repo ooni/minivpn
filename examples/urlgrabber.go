4b4782ff67b5617da235c568d211e105c0c2f3d1
Ain Ghazal <ainghazal42@gmail.com>
2022-04-29 21:47:41 +0200

    profile url grab

--- examples/urlgrabber.go
+++ examples/urlgrabber.go
@@ -12,6 +12,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -19,9 +20,29 @@ import (
 	"os"
 
 	"github.com/ainghazal/minivpn/vpn"
+
+	"runtime"
+	"runtime/pprof"
 )
 
+var cpuprofile = flag.String("cpuprof", "", "write cpu profile to `file`")
+var url = flag.String("url", "", "url to fetch")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		runtime.SetCPUProfileRate(60)
+		log.Println("creating cpu profile at:", *cpuprofile)
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 	provider := os.Getenv("PROVIDER")
 	if provider == "" {
 		log.Fatal("Export the PROVIDER variable")
@@ -43,12 +64,12 @@ func main() {
 	// BUG(ainghazal): https stalls unless I tweak the tun-mtu that the
 	// remote announces. I might want to look at the mtu discovery that
 	// openvpn does.
-	if len(os.Args) != 2 {
-		log.Println("Usage: get <https://foobar>")
-		os.Exit(1)
-	}
-	uri := os.Args[1]
-	resp, err := client.Get(uri)
+	//if len(os.Args) != 2 {
+	//	log.Println("Usage: get <https://foobar>")
+	//	os.Exit(1)
+	//}
+
+	resp, err := client.Get(*url)
 	if err != nil {
 		log.Panic(err)
 	}
