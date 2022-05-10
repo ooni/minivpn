bd2aeb9c1fdf7ea1a75846f9874beaa508bf3153
Ain Ghazal <ainghazal42@gmail.com>
2022-05-10 21:57:17 +0200

    move keys funcs to data.go

--- vpn/data.go
+++ vpn/data.go
@@ -76,6 +76,43 @@ func (dck *dataChannelKey) addRemoteKey(k *keySource) error {
 	return nil
 }
 
+var (
+	randomFn       = genRandomBytes
+	errRandomBytes = errors.New("Error generating random bytes")
+)
+
+// random data to generate keys
+type keySource struct {
+	r1        []byte
+	r2        []byte
+	preMaster []byte
+}
+
+func (k *keySource) Bytes() []byte {
+	r := append(k.preMaster, k.r1...)
+	return append(r, k.r2...)
+}
+
+func newKeySource() (*keySource, error) {
+	r1, err := randomFn(32)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", errRandomBytes, err.Error())
+	}
+	r2, err := randomFn(32)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", errRandomBytes, err.Error())
+	}
+	preMaster, err := randomFn(48)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", errRandomBytes, err.Error())
+	}
+	return &keySource{
+		r1:        r1,
+		r2:        r2,
+		preMaster: preMaster,
+	}, nil
+}
+
 // newDataFromOptions returns a new data object, initialized with the
 // options given. it also returns any error raised.
 func newDataFromOptions(opt *Options, s *session) (*data, error) {
