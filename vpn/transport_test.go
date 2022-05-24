e677f59ab63b999c50fe5df536130d1ec92b9157
Ain Ghazal <ainghazal42@gmail.com>
2022-05-24 11:14:00 +0200

    test tls trasnport proxy methods

--- vpn/transport_test.go
+++ vpn/transport_test.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/ainghazal/minivpn/vpn/mocks"
 )
@@ -92,5 +93,125 @@ func Test_NewTLSConn(t *testing.T) {
 	_, err := NewTLSConn(conn, s)
 	if err != nil {
 		t.Errorf("NewTLSConn() error = %v, want = nil", err)
+	}
+}
+
+type MockTLSConn struct {
+	mocks.Conn
+	closedCalled           bool
+	localAddrCalled        bool
+	remoteAddrCalled       bool
+	setDeadlineCalled      bool
+	setReadDeadlineCalled  bool
+	setWriteDeadlineCalled bool
+}
+
+func makeConnForTransportTest() *MockTLSConn {
+	localAddr := &mocks.Addr{}
+	localAddr.MockString = func() string { return "1.1.1.1" }
+	localAddr.MockNetwork = func() string { return "udp" }
+
+	remoteAddr := &mocks.Addr{}
+	remoteAddr.MockString = func() string { return "2.2.2.2" }
+	remoteAddr.MockNetwork = func() string { return "udp" }
+
+	c := &MockTLSConn{}
+	c.MockClose = func() error {
+		c.closedCalled = true
+		return nil
+	}
+	c.MockLocalAddr = func() net.Addr {
+		c.localAddrCalled = true
+		return localAddr
+	}
+	c.MockRemoteAddr = func() net.Addr {
+		c.remoteAddrCalled = true
+		return remoteAddr
+	}
+	c.MockSetDeadline = func(time.Time) error {
+		c.setDeadlineCalled = true
+		return nil
+	}
+	c.MockSetReadDeadline = func(time.Time) error {
+		c.setReadDeadlineCalled = true
+		return nil
+	}
+	c.MockSetWriteDeadline = func(time.Time) error {
+		c.setWriteDeadlineCalled = true
+		return nil
+	}
+	return c
+}
+
+func makeTLSConn() (*TLSConn, *MockTLSConn) {
+	c := makeConnForTransportTest()
+	t := &TLSConn{}
+	t.conn = c
+	return t, c
+}
+
+func Test_TLSConn_Close(t *testing.T) {
+	tc, conn := makeTLSConn()
+	err := tc.Close()
+	if err != nil {
+		t.Errorf("TLSConn.Close() error = %v, want = nil", err)
+	}
+	if !conn.closedCalled {
+		t.Error("TLSConn.Close(): conn.Close() not called")
+	}
+}
+
+func Test_TLSConn_LocalAddr(t *testing.T) {
+	tc, conn := makeTLSConn()
+	want := "1.1.1.1"
+	if addr := tc.LocalAddr(); addr.String() != want {
+		t.Errorf("TLSConn.LocalAddr() got = %s, want = %s", addr, want)
+	}
+	if !conn.localAddrCalled {
+		t.Error("TLSConn.LocalAddr(): conn.LocalAddr() not called")
+	}
+}
+
+func Test_TLSConn_RemoteAddr(t *testing.T) {
+	tc, conn := makeTLSConn()
+	want := "2.2.2.2"
+	if addr := tc.RemoteAddr(); addr.String() != want {
+		t.Errorf("TLSConn.RemoteAddr() got = %s, want = %s", addr, want)
+	}
+	if !conn.remoteAddrCalled {
+		t.Error("TLSConn.RemoteAddr(): conn.RemoteAddr() not called")
+	}
+}
+
+func Test_TLSConn_SetDeadline(t *testing.T) {
+	tc, conn := makeTLSConn()
+	err := tc.SetDeadline(time.Now().Add(time.Second))
+	if err != nil {
+		t.Errorf("TLSConn.SetDeadline() error = %v, want = nil", err)
+	}
+	if !conn.setDeadlineCalled {
+		t.Error("TLSConn.SetDeadline(): conn.SetDeadline() not called")
+	}
+}
+
+func Test_TLSConn_SetReadDeadline(t *testing.T) {
+	tc, conn := makeTLSConn()
+	err := tc.SetReadDeadline(time.Now().Add(time.Second))
+	if err != nil {
+		t.Errorf("TLSConn.SetReadDeadline() error = %v, want = nil", err)
+	}
+	if !conn.setReadDeadlineCalled {
+		t.Error("TLSConn.SetReadDeadline(): conn.SetReadDeadline() not called")
+	}
+}
+
+func Test_TLSConn_SetWriteDeadline(t *testing.T) {
+	tc, conn := makeTLSConn()
+	err := tc.SetWriteDeadline(time.Now().Add(time.Second))
+	if err != nil {
+		t.Errorf("TLSConn.SetWriteDeadline() error = %v, want = nil", err)
+	}
+	if !conn.setWriteDeadlineCalled {
+		t.Error("TLSConn.SetWriteDeadline(): conn.SetWriteDeadline() not called")
 	}
 }
