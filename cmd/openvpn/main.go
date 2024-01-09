99ce0d546f21dacf6db215b586259e28d9e368d4
ain ghazal <ainghazal42@gmail.com>
2024-01-09 23:33:20 +0100

    add route for endpoint via default gateway

--- cmd/openvpn/main.go
+++ cmd/openvpn/main.go
@@ -13,28 +13,26 @@ import (
 	"github.com/ooni/minivpn/internal/tun"
 
 	"github.com/Doridian/water"
+	"github.com/jackpal/gateway"
 )
 
-func runIP(args ...string) {
-	cmd := exec.Command("/sbin/ip", args...)
+func runCmd(binaryPath string, args ...string) {
+	cmd := exec.Command(binaryPath, args...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	err := cmd.Run()
 	if nil != err {
-		log.WithError(err).Fatal("error running /sbin/ip")
+		log.WithError(err).Warn("error running /sbin/ip")
 	}
 }
 
+func runIP(args ...string) {
+	runCmd("/sbin/ip", args...)
+}
+
 func runRoute(args ...string) {
-	cmd := exec.Command("/sbin/route", args...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	err := cmd.Run()
-	if nil != err {
-		log.WithError(err).Fatal("error running /sbin/route")
-	}
+	runCmd("/sbin/route", args...)
 }
 
 func main() {
@@ -62,8 +60,8 @@ func main() {
 
 	// create a vpn tun Device
 	tunnel := tun.StartTUN(conn, options)
-	fmt.Println(tunnel.LocalAddr())
-	fmt.Println(tunnel.RemoteAddr())
+	fmt.Printf("Local IP: %s\n", tunnel.LocalAddr())
+	fmt.Printf("Gateway:  %s\n", tunnel.RemoteAddr())
 
 	// create a tun interface on the OS
 	iface, err := water.New(water.Config{
@@ -73,49 +71,89 @@ func main() {
 		log.WithError(err).Fatal("Unable to allocate TUN interface:")
 	}
 
+	// TODO: investigate what's the maximum working MTU, additionally get it from flag.
 	MTU := 1400
 	iface.SetMTU(MTU)
 
 	localAddr := tunnel.LocalAddr().String()
 	remoteAddr := tunnel.RemoteAddr().String()
 
-	// TODO: missing
-	// sudo route add 163.172.211.109 gw 192.168.18.1 enp38s0
+	// discover local gateway IP, to
+	defaultGatewayIP, err := gateway.DiscoverGateway()
+	if err != nil {
+		log.Warn("could not discover default gateway IP, routes might be broken")
+	}
+	defaultInterfaceIP, err := gateway.DiscoverInterface()
+	if err != nil {
+		log.Warn("could not discover default route interface IP, routes might be broken")
+	}
+	defaultInterface, err := getInterfaceByIP(defaultInterfaceIP.String())
+	if err != nil {
+		log.Warn("could not get default route interface, routes might be broken")
+	}
+
+	if defaultGatewayIP != nil && defaultInterface != nil {
+		log.Infof("route add %s gw %v dev %s", options.Remote, defaultGatewayIP, defaultInterface.Name)
+		runRoute("add", options.Remote, "gw", defaultGatewayIP.String(), defaultInterface.Name)
+	}
 
 	// configure the interface and bring it up
 	runIP("addr", "add", localAddr, "dev", iface.Name())
 	runIP("link", "set", "dev", iface.Name(), "up")
 	runRoute("add", remoteAddr, "gw", localAddr)
-	// TODO this has hardcoded network for UDP
-	runRoute("add", "-net", "10.42.0.0/21", "dev", iface.Name())
+	// TODO this has the hardcoded network for TCP
+	// To do it properly, we need to parse server options.
+	// We could also honor DNS etc.
+	// get: route-gateway[0] 10.41.0.1
+	// get: ifconfig[1] 255.255.248.0
+	runRoute("add", "-net", "10.41.0.0/21", "dev", iface.Name())
 	runIP("route", "add", "default", "via", remoteAddr, "dev", iface.Name())
-	fmt.Println("iface", iface)
 
 	go func() {
-		packet := make([]byte, 2000)
 		for {
+			packet := make([]byte, 2000)
 			n, err := iface.Read(packet)
 			if err != nil {
 				log.WithError(err).Fatal("error reading from tun")
 			}
 			tunnel.Write(packet[:n])
-			log.Infof("tun: packet received: % x\n", packet[:n])
+			//log.Infof("tun: packet received: % x\n", packet[:n])
 		}
 	}()
 	go func() {
-		packet := make([]byte, 2000)
 		for {
+			packet := make([]byte, 2000)
 			n, err := tunnel.Read(packet)
 			if err != nil {
 				log.WithError(err).Fatal("error reading from tun")
 			}
 			iface.Write(packet[:n])
-			log.Infof("tun: packet sent: % x\n", packet[:n])
+			//log.Infof("tun: packet sent: % x\n", packet[:n])
 		}
 	}()
 	select {}
 }
 
-// /sbin/route add 10.42.0.1 gw 10.42.0.9
-// /sbin/route add -net 10.42.0.0/24 dev tun0
-// /sbin/ip route add default via 10.42.0.1 dev tun0
+func getInterfaceByIP(ipAddr string) (*net.Interface, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, iface := range interfaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+
+		for _, addr := range addrs {
+			if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+				if ipNet.IP.String() == ipAddr {
+					return &iface, nil
+				}
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("interface with IP %s not found", ipAddr)
+}
