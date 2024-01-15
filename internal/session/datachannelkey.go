4d0ca1359e084e3ef46adcec527c60f8ec2efff7
Ain Ghazal <99027643+ainghazal@users.noreply.github.com>
2024-01-15 14:38:58 +0100

    refactor: introduce the packetmuxer layer (#49)
    This is the second commit in the series of incremental refactoring of
    the current minivpn tree.
    
    In this commit, we introduce the packet muxer, which is the layer just
    above the networkio. The packet muxer handles and routes data or control
    packets, and it also handles reset packets.
    
    As dependencies for `packetmuxer`, we're also introducing the `optional`
    and `session` packages.
    
    Reference issue: #47
    
    ---------
    
    Co-authored-by: Simone Basso <bassosimone@gmail.com>

---
+++ internal/session/datachannelkey.go
@@ -0,0 +1,57 @@
+package session
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+// DataChannelKey represents a pair of key sources that have been negotiated
+// over the control channel, and from which we will derive local and remote
+// keys for encryption and decrption over the data channel. The index refers to
+// the short key_id that is passed in the lower 3 bits if a packet header.
+// The setup of the keys for a given data channel (that is, for every key_id)
+// is made by expanding the keysources using the prf function.
+//
+// Do note that we are not yet implementing key renegotiation - but the index
+// is provided for convenience when/if we support that in the future.
+type DataChannelKey struct {
+	index  uint32
+	ready  bool
+	local  *KeySource
+	remote *KeySource
+	mu     sync.Mutex
+}
+
+// errDayaChannelKey is a [DataChannelKey] error.
+var errDataChannelKey = errors.New("bad data-channel key")
+
+// Local returns the local [KeySource]
+func (dck *DataChannelKey) Local() *KeySource {
+	return dck.local
+}
+
+// Remote returns the local [KeySource]
+func (dck *DataChannelKey) Remote() *KeySource {
+	return dck.remote
+}
+
+// AddRemoteKey adds the server keySource to our dataChannelKey. This makes the
+// dataChannelKey ready to be used.
+func (dck *DataChannelKey) AddRemoteKey(k *KeySource) error {
+	dck.mu.Lock()
+	defer dck.mu.Unlock()
+	if dck.ready {
+		return fmt.Errorf("%w: %s", errDataChannelKey, "cannot overwrite remote key slot")
+	}
+	dck.remote = k
+	dck.ready = true
+	return nil
+}
+
+// Ready returns whether the [DataChannelKey] is ready.
+func (dck *DataChannelKey) Ready() bool {
+	dck.mu.Lock()
+	defer dck.mu.Unlock()
+	return dck.ready
+}
