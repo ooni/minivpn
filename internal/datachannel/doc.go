8addd7dba773915f581ead02b556db947202f66f
ain ghazal <ainghazal42@gmail.com>
2024-01-19 13:28:50 +0100

    add doc.go

---
+++ internal/datachannel/doc.go
@@ -0,0 +1,4 @@
+// Package datachannel implements packet encryption and decryption over the OpenVPN Data Channel.
+// Encryption Keys are derived after a successful TLS handshake, and they have a limited
+// lifetime.
+package datachannel
