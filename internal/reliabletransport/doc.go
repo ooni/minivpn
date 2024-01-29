1bc23611ae6c88b6ae5f02ac60eaca11c0f4e0e0
ain ghazal <ainghazal42@gmail.com>
2024-01-29 17:54:20 +0100

    add link in docs

--- internal/reliabletransport/doc.go
+++ internal/reliabletransport/doc.go
@@ -1,4 +1,9 @@
-// Package reliabletransport implements the reliable transport.
+// Package reliabletransport implements the reliable transport module for OpenVPN.
+// See [the official documentation](https://community.openvpn.net/openvpn/wiki/SecurityOverview) for a detail explanation
+// of why this is needed, and how it relates to the requirements of the control channel.
+// It is worth to mention that, even though the original need is to have a reliable control channel
+// on top of UDP, this is also used when tunneling over TCP.
+//
 // A note about terminology: in this package, "receiver" is the moveUpWorker in the [reliabletransport.Service] (since it receives incoming packets), and
 // "sender" is the moveDownWorker in the same service. The corresponding data structures lack mutexes because they are intended to be confined to a single
 // goroutine (one for each worker), and they SHOULD ONLY communicate via message passing.
