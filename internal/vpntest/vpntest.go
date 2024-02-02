0bcb8dec003129a035c2079b48f062707e8ad205
ain ghazal <ainghazal42@gmail.com>
2024-02-02 12:27:34 +0100

    parse acks

--- internal/vpntest/vpntest.go
+++ internal/vpntest/vpntest.go
@@ -2,6 +2,7 @@
 package vpntest
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -14,11 +15,14 @@ import (
 // have a compact representation of a sequence of packets, their type, and extra properties like
 // inter-arrival time.
 type TestPacket struct {
+	// Opcode is the OpenVPN packet opcode.
+	Opcode model.Opcode
+
 	// ID is the packet sequence
 	ID int
 
-	// Opcode is the OpenVPN packet opcode.
-	Opcode model.Opcode
+	// ACKs is the ack array in this packet
+	ACKs []int
 
 	// IAT is the inter-arrival time until the next packet is received.
 	IAT time.Duration
@@ -29,20 +33,30 @@ type TestPacket struct {
 func NewTestPacketFromString(s string) (*TestPacket, error) {
 	parts := strings.Split(s, " +")
 
-	// Extracting id and opcode parts
-	idAndOpcode := strings.Split(parts[0], " ")
-	if len(idAndOpcode) != 2 {
-		return nil, fmt.Errorf("invalid format for ID and opcode: %s", parts[0])
+	// Extracting id, opcode and ack parts
+	head := strings.Split(parts[0], " ")
+	if len(head) < 2 || len(head) > 3 {
+		return nil, fmt.Errorf("invalid format for ID-op-acks: %s", parts[0])
 	}
 
-	id, err := strconv.Atoi(strings.Trim(idAndOpcode[0], "[]"))
+	id, err := strconv.Atoi(strings.Trim(head[0], "[]"))
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse id: %v", err)
 	}
 
-	opcode, err := model.NewOpcodeFromString(idAndOpcode[1])
+	opcode, err := model.NewOpcodeFromString(head[1])
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse opcode: %v", err)
+	}
+
+	acks := []int{}
+
+	if len(head) == 3 {
+		acks, err = parseACKs(strings.Trim(head[2], "()"))
+		fmt.Println("acks:", acks)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse opcode: %v", err)
+		}
 	}
 
 	// Parsing duration part
@@ -52,5 +66,23 @@ func NewTestPacketFromString(s string) (*TestPacket, error) {
 		return nil, fmt.Errorf("failed to parse duration: %v", err)
 	}
 
-	return &TestPacket{ID: id, Opcode: opcode, IAT: iat}, nil
+	return &TestPacket{ID: id, Opcode: opcode, ACKs: acks, IAT: iat}, nil
+}
+
+var errBadACK = errors.New("wrong ack string")
+
+func parseACKs(s string) ([]int, error) {
+	acks := []int{}
+	h := strings.Split(s, "ack:")
+	if len(h) != 2 {
+		return acks, errBadACK
+	}
+	values := strings.Split(h[1], ",")
+	for _, v := range values {
+		n, err := strconv.Atoi(v)
+		if err == nil {
+			acks = append(acks, n)
+		}
+	}
+	return acks, nil
 }
--- internal/vpntest/vpntest_test.go
+++ internal/vpntest/vpntest_test.go
@@ -25,6 +25,29 @@ func TestNewTestPacketFromString(t *testing.T) {
 			want: &TestPacket{
 				ID:     1,
 				Opcode: model.P_CONTROL_V1,
+				ACKs:   []int{},
+				IAT:    time.Millisecond * 42,
+			},
+			wantErr: false,
+		},
+		{
+			name: "parse a testpacket with acks",
+			args: args{"[1] CONTROL_V1 (ack:0,1) +42ms"},
+			want: &TestPacket{
+				ID:     1,
+				Opcode: model.P_CONTROL_V1,
+				ACKs:   []int{0, 1},
+				IAT:    time.Millisecond * 42,
+			},
+			wantErr: false,
+		},
+		{
+			name: "empty acks part",
+			args: args{"[1] CONTROL_V1 (ack:) +42ms"},
+			want: &TestPacket{
+				ID:     1,
+				Opcode: model.P_CONTROL_V1,
+				ACKs:   []int{},
 				IAT:    time.Millisecond * 42,
 			},
 			wantErr: false,
